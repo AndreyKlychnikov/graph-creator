bf8f704b9baca6d46a279907dc54913bbf168427
andrew <off-rey@bk.ru>
2021-10-02 14:17:57 +0300

    Add graph creator with edit functional.

---
+++ src/components/Graph/config.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+export const NODE_KEY = "id";
+
+export const CUSTOM_EMPTY_TYPE = "customEmpty";
+export const POLY_TYPE = "poly";
+export const EMPTY_EDGE_TYPE = "emptyEdge";
+
+export const nodeTypes = [
+  CUSTOM_EMPTY_TYPE,
+  POLY_TYPE,
+];
+export const edgeTypes = [EMPTY_EDGE_TYPE];
+
+
+const CustomEmptyShape = (
+  <symbol viewBox="0 0 200 200" id="customEmpty">
+    <circle cx="100" cy="100" r="50"  />
+  </symbol>
+);
+
+const PolyShape = (
+  <symbol viewBox="0 0 88 72" id="poly" width="88" height="88">
+    <path d="M 0 36 18 0 70 0 88 36 70 72 18 72Z" />
+  </symbol>
+);
+
+const EmptyEdgeShape = (
+  <symbol viewBox="0 0 50 50" id="emptyEdge">
+    <circle cx="25" cy="25" r="17" fill="currentColor" />
+  </symbol>
+);
+
+
+export default {
+  EdgeTypes: {
+    emptyEdge: {
+      shape: EmptyEdgeShape,
+      shapeId: "#emptyEdge",
+      typeText: "Empty"
+    }
+  },
+  NodeSubtypes: {},
+  NodeTypes: {
+    customEmpty: {
+      shape: CustomEmptyShape,
+      shapeId: "#customEmpty",
+      typeText: "Node"
+    },
+    poly: {
+      shape: PolyShape,
+      shapeId: "#poly",
+      typeText: "Poly"
+    }
+  }
+};
